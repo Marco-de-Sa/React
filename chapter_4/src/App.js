c5e1b15d2e0791f620b3551b66837de662708707
Marco-de-Sa <Marcojb.desa@gmail.com>
2023-08-10 12:16:06 +0200

    chapter 4 final fix

--- chapter_4/src/App.js
+++ chapter_4/src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from "react";
-// import Products from "./Products";
-// import { Button } from "react-bootstrap";
-import Rating from './Rating';
+import React from 'react';
+import Products from './Products';
 
-
-class App extends Component {
-  render() {
-    // const isValid = true;
-    return (
-      <div>
-        {/* <h1>My First React App!</h1>
-        <Products />
-        <Button variant="primary" disabled={!isValid}>Default</Button> */}
-
-        <Rating rating="1" />
-        <Rating rating="2" />
-        <Rating rating="3" />
-        <Rating rating="4" />
-        <Rating rating="5" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
 }
+
 export default App;
