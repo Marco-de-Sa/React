6664396d7c1fd25a0d0f509c47c3d40857f2df17
Marco-de-Sa <Marcojb.desa@gmail.com>
2023-08-10 11:32:05 +0200

    chapter 4 and 5

--- chapter_4/src/Product.js
+++ chapter_4/src/Product.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
 import Rating from './Rating';
-import { Media } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 class Product extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-  constructor(props){
-    super(props);    
-  }
-
-  render() { 
-    return (
-      <div>                
-        <Media>
-            <img 
-                width={64} 
-                height={64} 
-                className="mr-3"
-                src={this.props.data.imageUrl} 
-                alt="Image"
-            />
-            <Media.Body>
-                <h5>{this.props.data.productName}</h5>
-                { this.props.data.releasedDate }
-                <Rating rating={this.props.data.rating} numOfReviews={this.props.data.numOfReviews}/>
-                <p>{this.props.data.description}</p>
-            </Media.Body>
-        </Media>                                                                                                                                   
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.data.imageUrl} />
+                    <Card.Body>
+                        <h5>{this.props.data.productName}</h5>
+                        {this.props.data.releasedDate}
+                        <Rating
+                            rating={this.props.data.rating}
+                            numOfReviews={this.props.data.numOfReviews}
+                        />
+                        <p>{this.props.data.description}</p>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
 }
 
 export default Product;
