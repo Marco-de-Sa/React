e0566586c347f426d794368ab73f20f4e005bad8
Marco-de-Sa <Marcojb.desa@gmail.com>
2023-08-28 12:33:59 +0200

    shift the code to call GitHub to componentDidMount

--- chapter_7/src/GitHub.js
+++ chapter_7/src/GitHub.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios'; // npm install axios
+
 class GitHub extends Component {
     constructor() {
         super();
+        this.state = {
+            isLoading: true
+        };
+    }
+
+    componentDidMount() {
         this.getGitHubData('greg');
     }
+
     getGitHubData(_searchTerm) {
         axios.get("https://api.github.com/search/users?q=" + _searchTerm).then(res => {
             console.log(res.data.items);
         });
     }
-    
+
     render() {
         return (
             <div>
