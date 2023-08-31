6dcd9bef50f8a90aadbccc82f15e755bef454778
Marco-de-Sa <Marcojb.desa@gmail.com>
2023-08-31 11:19:04 +0200

    chapter 7 final fixes

--- chapter_7/src/App.js
+++ chapter_7/src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import GitHub from './GitHub';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
+
   render() {
     return (
       <div>
+        <GitHub />
         {this.state.isLoading &&
           <h4>Getting data...</h4>
         }
